690d5dc5151e287264f329f824d34dcd28ef696c
DMau1420 <shootingdarcklight@gmail.com>
2026-07-27 13:09:48 -0600

    feat: Funcionalidad de docker con bd de prueba y fastapi con entorno virutual uv correcto

--- backend/prueba/main.py
+++ backend/prueba/main.py
@@ -1,5 +1,25 @@
 import random
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String, Numeric
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+
+engine = create_engine("postgresql+psycopg2://admin:adminpassword@localhost:5432/ecommerce_db")
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Producto(Base):
+    __tablename__ = "productos"
+    producto_id = Column(Integer, primary_key=True)
+    nombre = Column(String)
+    precio = Column(Numeric)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -11,3 +31,7 @@ def saludar():
 def ping():
     res = f"{random.randint(0,255)}ms"
     return {"ping": res}
+
+@app.get("/productos")
+def obtener_productos(db: Session = Depends(get_db)):
+    return db.query(Producto).all()
