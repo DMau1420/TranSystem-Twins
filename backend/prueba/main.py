06f78a1c4ec001fd93b502bc20581de31823e047
DMau1420 <shootingdarcklight@gmail.com>
2026-07-30 22:49:32 -0600

    feat: Prueba de generacion de json para simulacion sumo mediante endpoint get-sumo-json

--- backend/prueba/main.py
+++ backend/prueba/main.py
@@ -21,6 +21,7 @@ def get_db():
     finally:
         db.close()
 
+
 app = FastAPI()
 
 @app.get("/")
@@ -35,3 +36,33 @@ def ping():
 @app.get("/productos")
 def obtener_productos(db: Session = Depends(get_db)):
     return db.query(Producto).all()
+
+@app.get("/get-sumo-json")
+def generar_json_sumo():
+    we = round(random.random(),2) 
+    ew = round(random.random(),2) 
+    ns = round(random.random(),2) 
+    d1 = random.randint(1,100)
+    d2 = random.randint(1,100)
+    d3 = random.randint(1,100)
+    d4 = random.randint(1,100)
+    estado_list = ["GrGr", "YrYr", "rGrG", "ryry"]
+    random.shuffle(estado_list)
+    e1 = estado_list.pop()
+    e2 = estado_list.pop()
+    e3 = estado_list.pop()
+    e4 = estado_list.pop()
+    seed = random.randint(0,33550336)
+    res_json = {"demanda": 
+                {"we":we,"ew":ew,"ns":ns},
+                "semaforo": {
+                    "fases":[
+                        {"duracion":d1, "estado":e1},
+                        {"duracion":d2, "estado":e2},
+                        {"duracion":d3, "estado":e3},
+                        {"duracion":d4, "estado":e4}
+                        ]
+                    },
+                "simulacion":{"duracion":3600,"seed":seed}
+                }
+    return res_json
